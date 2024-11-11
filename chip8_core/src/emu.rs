fb2e20ab2e7a2bc8228b3802e38373ce52eec0dc
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-11-11 11:12:46 +0200

    feat: added emulator main loop primitives

--- chip8_core/src/emu.rs
+++ chip8_core/src/emu.rs
@@ -21,7 +21,7 @@ pub struct Emu {
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut emu = Self {
             pc: SPECS.start_addr,
             ram: [0; SPECS.ram_size],
             screen: [false; SPECS.screen_w * SPECS.screen_h],
@@ -32,6 +32,61 @@ impl Emu {
             keys: [false; SPECS.keys_count],
             dt: 0,
             st: 0,
+        };
+
+        emu.ram[..SPECS.fontset_size].copy_from_slice(&SPECS.fontset);
+
+        emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = SPECS.start_addr;
+        self.ram = [0; SPECS.ram_size];
+        self.screen = [false; SPECS.screen_w * SPECS.screen_h];
+        self.v_reg = [0; SPECS.registers_count];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; SPECS.stack_size];
+        self.keys = [false; SPECS.keys_count];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..SPECS.fontset_size].copy_from_slice(&SPECS.fontset)
+    }
+
+    pub fn tick(&mut self) {
+        let op = self.fetch();
+        // decode
+        // exec
+    }
+
+    pub fn tick_timer(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
         }
+
+        if self.st > 0 {
+            if self.st == 1 {
+                // beep
+            }
+            self.st -= 1;
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let high_byte = self.ram[self.pc as usize] as u16;
+        let low_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (high_byte << 8) | low_byte;
+        self.pc += 2;
+        op
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
     }
 }
